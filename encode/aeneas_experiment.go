5f10d576a607d7abc3a18936d5bf56d8811ed249
Gary N Griswold <gary@shortsands.com>
2026-06-30 21:38:20 -0700

    clean up an import

--- encode/aeneas_experiment.go
+++ encode/aeneas_experiment.go
@@ -3,16 +3,17 @@ package encode
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
 	"github.com/artificial-polyglot/arti/db"
 	"github.com/artificial-polyglot/arti/decode_yaml/request"
 	"github.com/artificial-polyglot/arti/input"
 	log "github.com/artificial-polyglot/arti/logger"
 	"github.com/artificial-polyglot/arti/read"
 	"github.com/artificial-polyglot/arti/xxxtobedeleted/cli_misc"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 )
 
 // This unfinished code was an attempt to organize scripts by verse in order
--- tests/99_run_anything_test.go
+++ tests/99_run_anything_test.go
@@ -1,41 +1,36 @@
 package tests
 
-import "testing"
+import (
+	"testing"
 
-const runAnything = `is_new: yes 
-dataset_name: N2MZJSIM
-language_iso: mzj
-username: GaryNTest 
-notify_ok: [gary@shortsands.com]
-notify_err: [gary@shortsands.com]
-testament:
-  nt_books: [3JN]
+	log "github.com/artificial-polyglot/arti/logger"
+)
+
+const runAnything = `is_new: yes
+dataset_name: N2QAEBSP
+username: GaryNTest
+language_iso: qae
+notify_ok: [ntfy/artificial-polyglot]
+notify_err: [ntfy/artificial-polyglot,gary@shortsands.com]
 text_data:
-  aws_s3: s3://pretest-audio/N2MZJSIM Manya (MZJ)/N2MZJSIM USX/*.usx
+  file: /Users/gary/arti2/fcbh_data/Dawasamu N2QAEBSP (Gospels)/Text Files/SFM Text/*.SFM
 audio_data:
-  aws_s3: s3://pretest-audio/N2MZJSIM Manya (MZJ)/N2MZJSIM Chapter VOX/*.mp3
-speech_to_text:
-  mms_asr_align: yes
-compare:
-  html_report: yes
-  gordon_filter: 4
-  compare_settings: 
-    lower_case: y
-    remove_prompt_chars: y
-    remove_punctuation: y
-    double_quotes: 
-      remove: y
-    apostrophe: 
-      remove: y
-    hyphen:
-      remove: y
-    diacritical_marks:
-      normalize_nfc: y
-output:
-  csv: y
+  file: /Users/gary/arti2/fcbh_data/Dawasamu N2QAEBSP (Gospels)/N2QAEBSP Chapter VOX/*.mp3
+timestamps:
+  mms_fa_verse: yes
+  mms_align: no
+training:
+  redo_training: yes
+  mms_adapter:
+    batch_mb: 4
+    num_epochs: 1
+    learning_rate: 1e-3
+    warmup_pct: 12.0
+    grad_norm_max: 0.4
 `
 
 func TestRunAnything(t *testing.T) {
 	var yaml = runAnything
+	log.SetOutput("stderr")
 	DirectSqlTest(yaml, []SqliteTest{}, t)
 }
