36edff558a36721e15cd085ef3d0085a4975b57b
Gary N Griswold <gary@shortsands.com>
2026-08-17 10:16:29 -0700

    qa_aligns use of fa does not support mps device

--- cmd/speech_to_text/qa_align/qa_align.py
+++ cmd/speech_to_text/qa_align/qa_align.py
@@ -47,7 +47,7 @@ adapter = len(sys.argv) > 2 and sys.argv[2].lower() == "adapter"
 if torch.cuda.is_available():
     device = 'cuda'
 elif torch.backends.mps.is_available():
-    device = 'mps'
+    device = 'cpu' # not implemented
 else:
     device = 'cpu'
 modelId = "facebook/mms-1b-all"
