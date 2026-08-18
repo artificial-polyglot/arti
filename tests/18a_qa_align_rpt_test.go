95da57235dbe1c247eeea64e14b273053113a17f
Gary N Griswold <gary@shortsands.com>
2026-08-17 22:56:23 -0700

    Add comment to 18a

--- tests/18a_qa_align_rpt_test.go
+++ tests/18a_qa_align_rpt_test.go
@@ -19,7 +19,7 @@ output:
   directory: ~/Downloads
   csv: yes
 testament:
-  nt_books: [3JN]
+  nt_books: [3JN] # 1JN, 2JN, 3JN, JUD all in test set
 database:
   aws_s3: s3://arti-input/17a_mms_adapter_test_data/17a_mms_adapter.db
 audio_data:
