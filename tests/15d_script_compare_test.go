884538c38a3335eddc9bdccc75c1370e095f5227
Gary N Griswold <gary@shortsands.com>
2026-08-18 15:39:21 -0700

    Update Tests

--- tests/15d_script_compare_test.go
+++ tests/15d_script_compare_test.go
@@ -2,21 +2,25 @@ package tests
 
 import (
 	"testing"
+
+	"github.com/artificial-polyglot/arti/courier"
 )
 
 const scriptCompare = `is_new: no
 dataset_name: 15d_script_compare_audio
 bible_id: IKHMLT
-username: GaryNTest
+username: Tests
+notify_err: [ntfy/arti2]
 output:
-  sqlite: yes
+  directory: ~/Download
+  csv: yes
+database:
+  aws_s3: s3://arti-input/15d_script_compare_audio_test_data/15d_script_compare_audio.db
 testament:
   nt: yes
-audio_proof:
-  html_report: no # must eliminate directory input files passed into module to do this
 compare:
   html_report: yes
-  base_dataset: 15d_script_compare
+  base_dataset: s3://arti-input/15d_script_compare_audio_test_data/15d_script_compare.db
   compare_settings: 
     lower_case: y
     remove_prompt_chars: y
@@ -32,5 +36,9 @@ compare:
 `
 
 func TestScriptCompare(t *testing.T) {
+	courier.IsCourierTest = true
+	var tests []SqliteTest
+	tests = append(tests, SqliteTest{"SELECT count(*) FROM scripts", 5112})
+	tests = append(tests, SqliteTest{"SELECT count(*) FROM scripts WHERE script_begin_ts != 0.0", 4995})
 	DirectSqlTest(scriptCompare, []SqliteTest{}, t)
 }
